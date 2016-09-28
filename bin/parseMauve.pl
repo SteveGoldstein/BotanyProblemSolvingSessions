5dc852a9095b46cd164f0e645b0df128ca6961ac
Steve Goldstein <sgoldstein@wisc.edu>
2016-09-28 14:42:52 -0500

    first few steps of parsing completed

--- bin/parseMauve.pl
+++ bin/parseMauve.pl
@@ -13,8 +13,21 @@ GetOptions (
             );
 
 while(<>) {
-  print;
-  last;
+  chomp;
+  next if (/^#/);
+  #print "$_\n";
+
+  if (/^> (\d+):/) {
+    # It's a seq header
+    print "$_\n";
+    print "got a header\n";
+  } ## if >
+
+  if (/^=/) {
+    print "$_\n";
+    print "got a separator\n";
+  } ## if =
+  #last;
 } ## while
 
 __END__
