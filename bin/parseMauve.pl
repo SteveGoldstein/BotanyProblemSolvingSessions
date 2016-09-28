8bd7761fe15b6939e6f451a2a3acc091d44b6745
Steve Goldstein <sgoldstein@wisc.edu>
2016-09-28 14:38:18 -0500

    first steps in parsing file

---
+++ bin/parseMauve.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+
+use warnings;
+use strict;
+use Carp;
+use English;
+use Getopt::Long;
+
+
+GetOptions (
+
+            );
+
+while(<>) {
+  print;
+  last;
+} ## while
+
+__END__
