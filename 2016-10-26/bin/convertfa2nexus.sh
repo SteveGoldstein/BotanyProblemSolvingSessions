0d206ebfa43db39e4b5cfed80a0b0f860457ff9f
Steve Goldstein <sgoldstein@wisc.edu>
2020-09-27 07:13:32 -0500

    fixing git

--- 2016-10-26/bin/convertfa2nexus.sh
+++ 2016-10-26/bin/convertfa2nexus.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-infile=data/lcbBlock1.fa
-outfile=nexus/lcbBlock1.nex
+dir=$1
+for f in $dir/*.fa 
+do
+  infile=$f
+  outfile=${infile/data/nexus}
+  outfile=${outfile/\.fa/.nex}
+  echo reformating $outfile...
+  java -cp src/readseq.jar run -o $outfile -f NEXUS $infile
+done
+exit
 
-java -cp src/readseq.jar run -o $outfile -f NEXUS $infile 
+while (<>) {
+
+}
+
+
